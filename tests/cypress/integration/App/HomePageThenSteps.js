3e803a98213b149d37d81024163c1f2ec1dd9f46
ewaannap <ewa.anna.p@gmail.com>
2021-06-23 07:45:00 +0200

    code improvments

--- tests/cypress/integration/App/HomePageThenSteps.js
+++ tests/cypress/integration/App/HomePageThenSteps.js
@@ -1,10 +1,6 @@
 import { Then } from "cypress-cucumber-preprocessor/steps";
 import HomePageViewDOM from "../../support/dom/HomePageViewDOM";
 
-// Then('I can see {string} item(s) in cart', (numberOfItems) => {
-//     HomePageViewDOM.topbar.getNumberOfItems.to.equal(numberOfItems)
-// })
-
 Then('I can see {string} item(s) in cart', numberOfItems => {
     HomePageViewDOM.topbar.getNumberOfItems.to.equal(numberOfItems)
 
